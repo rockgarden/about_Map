418fb7e1ca939aa92f3838c5d74779707db607e2
Wangkan <wangkan@wangkandemac-mini.local>
2016-04-14 15:42:07 +0800

    rre

---
+++ MemoryInMap/MemoryInMap/MapViewController.swift
@@ -0,0 +1,132 @@
+//
+//  MapViewController.swift
+//  Map-swift
+//
+//  Created by Gazolla on 25/07/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate{
+    
+    var venuePoints:[Int: MapPointAnnotation] = [Int:MapPointAnnotation]()
+    var map:MKMapView?
+    var venues: [Venue]?
+    var rightButton: UIButton?
+    var selectedVenue:Venue?
+    
+
+    convenience init(frame:CGRect){
+        self.init(nibName: nil, bundle: nil)
+        self.view.frame = frame
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(MapViewController.selectAnnotation(_:)), name: "selectAnnotation", object: nil)
+
+        self.map = MKMapView(frame: frame)
+        self.map!.delegate = self
+        
+        self.view.addSubview(self.map!)
+        
+
+       adjustRegion(37.3175,aLongitude: -122.0419)
+    }
+    
+    func adjustRegion(aLatitude:CLLocationDegrees, aLongitude: CLLocationDegrees){
+        let latitude:CLLocationDegrees = aLatitude
+        let longitude:CLLocationDegrees = aLongitude
+        let latDelta:CLLocationDegrees = 1.0
+        let longDelta:CLLocationDegrees = 1.0
+        
+        let aSpan:MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta,longitudeDelta: longDelta)
+        let Center :CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(Center, aSpan)
+        
+        self.map!.setRegion(region, animated: true)
+    }
+    
+    func loadPointsWithArray(someVenues:[Venue]){
+        map!.removeAnnotations(map!.annotations)
+        for i in 0..<someVenues.count {
+    
+            let point:MapPointAnnotation = MapPointAnnotation()
+            let v = someVenues[i] as Venue
+            point.venue = v
+            let latitude = (v.lat as NSString).doubleValue
+            let longitude = (v.lng as NSString).doubleValue
+            point.coordinate = CLLocationCoordinate2DMake(latitude,longitude);
+            point.title =  v.name
+            point.subtitle = v.categoryName
+            venuePoints[v.ident] = point
+    
+            map!.addAnnotation(point)
+        }
+    }
+    
+    // select venue from tableview
+    func selectAnnotation(notification :NSNotification)  {
+        self.selectedVenue = notification.object as? Venue
+        let point:MKPointAnnotation = venuePoints[self.selectedVenue!.ident]!
+        map!.selectAnnotation(point, animated: true)
+    }
+    
+    //select venue from mapview
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        
+        let p = view.annotation as! MapPointAnnotation
+        self.selectedVenue = p.venue
+        print("\(p.venue)")
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.animatesDrop = true
+            pinView!.pinColor = .Purple
+        
+            
+            if self.rightButton == nil {
+                self.rightButton = UIButton(type: UIButtonType.DetailDisclosure) 
+            }
+           // let point:MapPointAnnotation = pinView!.annotation as! MapPointAnnotation
+           // println("point.venue.name = \(point.venue?.name)")
+          //  self.rightButton!.venue = point.venue
+            self.rightButton!.titleForState(UIControlState.Normal)
+            
+          self.rightButton!.addTarget(self, action: #selector(MapViewController.rightButtonTapped(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+            pinView!.rightCalloutAccessoryView = self.rightButton! as UIView
+   
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+
+
+    func rightButtonTapped(sender: UIButton!){
+        if let venue:Venue = selectedVenue{
+            
+            print("venue name:\(venue.name)")
+            
+            NSNotificationCenter.defaultCenter().postNotificationName("navigateToDetail", object: venue)
+        } else {
+            print("no venue")
+        }
+    }
+    
+    
+}
---
+++ MemoryInMap/MemoryInMap/TableMapViewController.swift
@@ -0,0 +1,136 @@
+//
+//  TableMapViewController.swift
+//  MapTable-Swift
+//
+//  Created by Gazolla on 18/07/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TableMapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+ 
+    var navHeight:CGFloat?
+    var width:CGFloat?
+    var halfHeight:CGFloat?
+    var height:CGFloat?
+    var firstPosition = true
+    var tableController:VenuesTableView?
+    var venues: Array<Venue> = [Venue]()
+    var mapView:MapViewController?
+    var tapFirstView:UIGestureRecognizer?
+    var bigMap = false
+    var detailVenue:VenueDetailViewController?
+
+    
+     convenience init(frame:CGRect){
+        self.init(nibName: nil, bundle: nil)
+        navHeight = 0.0
+        width = frame.size.width
+        halfHeight = (frame.size.height - navHeight!)/2
+        height = frame.size.height
+        
+        title = "Map & Table"
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "mapViewTapped", name: "mapViewTapped", object: nil)
+        
+        mapView = MapViewController(frame: CGRectMake(0.0, navHeight!, width!, halfHeight!))
+      
+        tapFirstView = UITapGestureRecognizer(target: self, action: "mapViewTapped")
+        mapView!.view.addGestureRecognizer(tapFirstView!)
+        self.view.addSubview(self.mapView!.view)
+        
+        tableController = VenuesTableView(frame: CGRectMake(0.0, halfHeight!, width!, halfHeight!))
+        view.addSubview(tableController!.view)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "navigateToDetail:", name: "navigateToDetail", object: nil)
+        
+        
+    }
+    
+    func mapViewTapped(){
+        if (!bigMap){
+            UIView.animateWithDuration(0.5,
+                delay: 0,
+                usingSpringWithDamping: 0.4,
+                initialSpringVelocity: 20.0,
+                options: UIViewAnimationOptions.CurveEaseIn ,
+                animations: {
+                    self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
+                    self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
+                    self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y+self.halfHeight!);
+                },
+                completion:{ (Bool)  in
+                    let leftBarButtonItem:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "back"), style: UIBarButtonItemStyle.Plain, target: self, action: "reverse")
+                    self.navigationItem.leftBarButtonItem = leftBarButtonItem
+                    self.bigMap = true
+                })
+        }
+    }
+    
+    func reverse(){
+        if bigMap {
+            UIView.animateWithDuration(0.5,
+                delay: 0,
+                usingSpringWithDamping: 0.4,
+                initialSpringVelocity: 20.0,
+                options: UIViewAnimationOptions.CurveEaseIn ,
+                animations: {
+                    self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
+                    self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
+                    self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y-self.halfHeight!);
+                },
+                completion:{ (Bool)  in
+                    self.navigationItem.leftBarButtonItem = nil
+                    self.bigMap = false
+                    
+                    if let selectedAnnotations = self.mapView!.map!.selectedAnnotations as? [MapPointAnnotation]{
+                        for annotation in selectedAnnotations {
+                            self.mapView!.map!.deselectAnnotation(annotation, animated: true)
+                        }
+                    }
+                })
+        }
+        
+    }
+
+    func setVenueCollection(array: [Venue]!) {
+         if (array != nil) {
+            venues = array!
+            tableController!.loadVenues(array!)
+            mapView!.loadPointsWithArray(array!)
+        }
+    }
+    
+    func navigateToDetail(notification:NSNotification){
+        
+        if self.detailVenue == nil {
+            self.detailVenue = VenueDetailViewController()
+        }
+        if let venue:Venue = notification.object as? Venue {
+            self.detailVenue?.lblName?.text = venue.name
+            self.detailVenue?.lblAddress?.text = venue.address
+            self.detailVenue?.lblCity?.text = venue.city
+        } else {
+            print("no venue at TableMapController")
+        }
+        self.navigationController?.pushViewController(self.detailVenue!, animated: true)
+        
+    }
+    
+    
+    
+}
