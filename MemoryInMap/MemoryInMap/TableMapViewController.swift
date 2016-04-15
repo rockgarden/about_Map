10a68d9b6f6f83f40be60ad18b729c356f3d8c76
Wangkan <wangkan@wangkandemac-mini.local>
2016-04-15 16:44:00 +0800

    add:extension

--- MemoryInMap/MemoryInMap/TableMapViewController.swift
+++ MemoryInMap/MemoryInMap/TableMapViewController.swift
@@ -21,8 +21,8 @@ class TableMapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
- 
+    
+    
     var navHeight:CGFloat?
     var width:CGFloat?
     var halfHeight:CGFloat?
@@ -34,9 +34,9 @@ class TableMapViewController: UIViewController {
     var tapFirstView:UIGestureRecognizer?
     var bigMap = false
     var detailVenue:VenueDetailViewController?
-
     
-     convenience init(frame:CGRect){
+    
+    convenience init(frame:CGRect){
         self.init(nibName: nil, bundle: nil)
         navHeight = 0.0
         width = frame.size.width
@@ -45,18 +45,18 @@ class TableMapViewController: UIViewController {
         
         title = "Map & Table"
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "mapViewTapped", name: "mapViewTapped", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(TableMapViewController.mapViewTapped), name: "mapViewTapped", object: nil)
         
         mapView = MapViewController(frame: CGRectMake(0.0, navHeight!, width!, halfHeight!))
-      
-        tapFirstView = UITapGestureRecognizer(target: self, action: "mapViewTapped")
+        
+        tapFirstView = UITapGestureRecognizer(target: self, action: #selector(TableMapViewController.mapViewTapped))
         mapView!.view.addGestureRecognizer(tapFirstView!)
         self.view.addSubview(self.mapView!.view)
         
         tableController = VenuesTableView(frame: CGRectMake(0.0, halfHeight!, width!, halfHeight!))
         view.addSubview(tableController!.view)
         
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "navigateToDetail:", name: "navigateToDetail", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(TableMapViewController.navigateToDetail(_:)), name: "navigateToDetail", object: nil)
         
         
     }
@@ -64,51 +64,51 @@ class TableMapViewController: UIViewController {
     func mapViewTapped(){
         if (!bigMap){
             UIView.animateWithDuration(0.5,
-                delay: 0,
-                usingSpringWithDamping: 0.4,
-                initialSpringVelocity: 20.0,
-                options: UIViewAnimationOptions.CurveEaseIn ,
-                animations: {
-                    self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
-                    self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
-                    self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y+self.halfHeight!);
+                                       delay: 0,
+                                       usingSpringWithDamping: 0.4,
+                                       initialSpringVelocity: 20.0,
+                                       options: UIViewAnimationOptions.CurveEaseIn ,
+                                       animations: {
+                                        self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
+                                        self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.height!)
+                                        self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y+self.halfHeight!);
                 },
-                completion:{ (Bool)  in
-                    let leftBarButtonItem:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "back"), style: UIBarButtonItemStyle.Plain, target: self, action: "reverse")
-                    self.navigationItem.leftBarButtonItem = leftBarButtonItem
-                    self.bigMap = true
-                })
+                                       completion:{ (Bool)  in
+                                        let leftBarButtonItem:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "back"), style: UIBarButtonItemStyle.Plain, target: self, action: #selector(TableMapViewController.reverse))
+                                        self.navigationItem.leftBarButtonItem = leftBarButtonItem
+                                        self.bigMap = true
+            })
         }
     }
     
     func reverse(){
         if bigMap {
             UIView.animateWithDuration(0.5,
-                delay: 0,
-                usingSpringWithDamping: 0.4,
-                initialSpringVelocity: 20.0,
-                options: UIViewAnimationOptions.CurveEaseIn ,
-                animations: {
-                    self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
-                    self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
-                    self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y-self.halfHeight!);
+                                       delay: 0,
+                                       usingSpringWithDamping: 0.4,
+                                       initialSpringVelocity: 20.0,
+                                       options: UIViewAnimationOptions.CurveEaseIn ,
+                                       animations: {
+                                        self.mapView!.view.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
+                                        self.mapView!.map!.frame = CGRectMake(0.0, self.navHeight!, self.width!, self.halfHeight!)
+                                        self.tableController!.view.center = CGPointMake(self.tableController!.view.center.x, self.tableController!.view.center.y-self.halfHeight!);
                 },
-                completion:{ (Bool)  in
-                    self.navigationItem.leftBarButtonItem = nil
-                    self.bigMap = false
-                    
-                    if let selectedAnnotations = self.mapView!.map!.selectedAnnotations as? [MapPointAnnotation]{
-                        for annotation in selectedAnnotations {
-                            self.mapView!.map!.deselectAnnotation(annotation, animated: true)
-                        }
-                    }
-                })
+                                       completion:{ (Bool)  in
+                                        self.navigationItem.leftBarButtonItem = nil
+                                        self.bigMap = false
+                                        
+                                        if let selectedAnnotations = self.mapView!.map!.selectedAnnotations as? [MapPointAnnotation]{
+                                            for annotation in selectedAnnotations {
+                                                self.mapView!.map!.deselectAnnotation(annotation, animated: true)
+                                            }
+                                        }
+            })
         }
         
     }
-
+    
     func setVenueCollection(array: [Venue]!) {
-         if (array != nil) {
+        if (array != nil) {
             venues = array!
             tableController!.loadVenues(array!)
             mapView!.loadPointsWithArray(array!)
