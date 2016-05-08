854749d551f460251a774dfbbfaf1ca66988d0eb
rockgarden <rockgarden@sina.com>
2016-05-08 22:15:17 +0800

    add: TableCell

---
+++ MemoryInMap/MemoryInMap/MapCell.swift
@@ -0,0 +1,24 @@
+//
+//  MapCell.swift
+//  MemoryInMap
+//
+//  Created by wangkan on 16/5/8.
+//  Copyright © 2016年 wangkan. All rights reserved.
+//
+
+import UIKit
+
+class MapCell: FoldingCell {
+
+    override func awakeFromNib() {
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        super.awakeFromNib()
+    }
+
+    override func animationDuration(itemIndex:NSInteger, type:AnimationType)-> NSTimeInterval {
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+    
+}
--- MemoryInMap/MemoryInMap/VenuesTableView.swift
+++ MemoryInMap/MemoryInMap/VenuesTableView.swift
@@ -13,6 +13,10 @@ class VenuesTableView: UITableViewController {
     var venues: [Venue] = []
     var rightButton:UIButton?
     let cellId = "cell"
+
+    let kCloseCellHeight: CGFloat = 179
+    let kOpenCellHeight: CGFloat = 488
+    var cellHeights = [CGFloat]()
      
     convenience init(frame:CGRect){
         self.init(style:.Plain)
@@ -30,25 +34,53 @@ class VenuesTableView: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        createCellHeightsArray()
+        self.tableView.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
     }
-    
+
+    // MARK: configure
+    func createCellHeightsArray() {
+        for _ in 0...venues.count {
+            cellHeights.append(kCloseCellHeight)
+        }
+    }
+
+    // MARK: - Table view data source
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return self.venues.count as Int
     }
+
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if cell is FoldingCell {
+            let foldingCell = cell as! FoldingCell
+            foldingCell.backgroundColor = UIColor.clearColor()
+
+            if cellHeights[indexPath.row] == kCloseCellHeight {
+                foldingCell.selectedAnimation(false, animated: false, completion:nil)
+            } else {
+                foldingCell.selectedAnimation(true, animated: false, completion: nil)
+            }
+        }
+    }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellId, forIndexPath: indexPath) 
-        
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.cellId, forIndexPath: indexPath)
+        // TODO:使用FoldingCell
+//        let cell = tableView.dequeueReusableCellWithIdentifier("FoldingCell", forIndexPath: indexPath)
         let venue = self.venues[indexPath.row] as Venue
         cell.textLabel!.text = venue.name
         debugPrint ("venue category: \(venue.categoryName)")
-    //    cell.detailTextLabel!.text = venue.categoryName
+//        cell.detailTextLabel!.text = venue.categoryName
         return cell
     }
-   
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+
+    // MARK: Table vie delegate
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         debugPrint (indexPath.row)
         let cell = self.tableView.cellForRowAtIndexPath(indexPath) as UITableViewCell?
@@ -57,4 +89,30 @@ class VenuesTableView: UITableViewController {
         let venue:Venue = self.venues[indexPath.row] as Venue
         NSNotificationCenter.defaultCenter().postNotificationName("selectAnnotation", object: venue)
     }
+
+//    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//
+//        let cell = tableView.cellForRowAtIndexPath(indexPath) as! FoldingCell
+//
+//        if cell.isAnimating() {
+//            return
+//        }
+//
+//        var duration = 0.0
+//        if cellHeights[indexPath.row] == kCloseCellHeight { // open cell
+//            cellHeights[indexPath.row] = kOpenCellHeight
+//            cell.selectedAnimation(true, animated: true, completion: nil)
+//            duration = 0.5
+//        } else {// close cell
+//            cellHeights[indexPath.row] = kCloseCellHeight
+//            cell.selectedAnimation(false, animated: true, completion: nil)
+//            duration = 0.8
+//        }
+//
+//        UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseOut, animations: { () -> Void in
+//            tableView.beginUpdates()
+//            tableView.endUpdates()
+//            }, completion: nil)
+//    }
+
 }
