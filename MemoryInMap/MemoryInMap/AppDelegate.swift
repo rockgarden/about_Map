f3644e7925b5b24d181f43585b648f6a477b2281
rockgarden <rockgarden@sina.com>
2016-06-04 13:06:21 +0800

    add

--- MemoryInMap/MemoryInMap/AppDelegate.swift
+++ MemoryInMap/MemoryInMap/AppDelegate.swift
@@ -37,6 +37,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         photosArr = [v,v2,v3,v4,v5,v6,v7]
     }
 
+    func requestsUserPermission() {
+        ICanHas.Location { (authorized, status) -> Void in
+
+            self.locationLabel.text = authorized ? "YES" : "NO"
+
+        }
+    }
+
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
