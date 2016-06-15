b746c3b0de5b8d92effe126d2e69642c66c582e7
rockgarden <rockgarden@sina.com>
2016-06-15 23:07:49 +0800

    add

---
+++ MemoryInMap/MemoryInMap/BaseUIViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseUIViewController.swift
+//  MemoryInMap
+//
+//  Created by wangkan on 16/6/15.
+//  Copyright © 2016年 Rockgarden. All rights reserved.
+//
+
+import UIKit
+
+public class BaseUIViewController: UIViewController {
+    
+    var showStatusBar = true
+
+    // MARK:- init
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    override public func prefersStatusBarHidden() -> Bool {
+        if self.showStatusBar {
+            return false
+        }
+        return true
+    }
+    
+    private func showStatusBar(bool: Bool) {
+        showStatusBar = bool
+        prefersStatusBarHidden()
+    }
+    
+}
