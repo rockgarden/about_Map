e7f350659d9323e70db1a42a465529f528187479
rockgarden <rockgarden@sina.com>
2016-06-25 21:44:13 +0800

    sync

---
+++ Pods/Target Support Files/KMNavigationBarTransition/KMNavigationBarTransition-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "KMSwizzle.h"
+#import "KMWeakObjectContainer.h"
+#import "UINavigationController+KMNavigationBarTransition.h"
+#import "UIViewController+KMNavigationBarTransition.h"
+
+FOUNDATION_EXPORT double KMNavigationBarTransitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char KMNavigationBarTransitionVersionString[];
+
