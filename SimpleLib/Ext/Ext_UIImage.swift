4f80054cfafbc60a9a86f4f83b57ed2bda5c8219
rockgarden <rockgarden@sina.com>
2016-07-21 23:02:50 +0800

    add func

--- SimpleLib/Ext/Ext_UIImage.swift
+++ SimpleLib/Ext/Ext_UIImage.swift
@@ -147,7 +147,20 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return image
     }
-    
+
+    class func createImageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRectMake(0.0, 0.0, 1.0, 1.0)
+        UIGraphicsBeginImageContext(rect.size);
+        let context = UIGraphicsGetCurrentContext()
+
+        CGContextSetFillColorWithColor(context, color.CGColor)
+        CGContextFillRect(context, rect)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return image
+    }
 }
 
 
